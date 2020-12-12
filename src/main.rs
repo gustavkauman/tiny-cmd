9fb4375cbf26c9bb61552a3b687e9a908353c916
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-12-12 15:06:59 +0100

    Add error handling and bump version no

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-extern crate clap;
-extern crate reqwest;
-
 use clap::{Arg, App};
-use reqwest::*;
+use reqwest::blocking::Client;
 use std::collections::HashMap;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() {
     let matches = App::new("tiny")
-        .version("0.1.0")
+        .version("1.0.0")
         .author("Gustav Utke Kauman <gkauman@eadu.dk>")
         .about("Command line tool for creating tiny URLs using the tinyurl.com API")
         .arg(Arg::with_name("Long URL")
@@ -18,25 +14,64 @@ async fn main() -> Result<()> {
             .help("URL to shorten"))
         .get_matches();
 
-    let url = matches.values_of("Long URL").unwrap().next().unwrap();
+    let value = match matches.values_of("Long URL") {
+        Some(mut value) => { value.next() }
+        None => {
+            println!("An error occured while processing the parameters given. Please make sure, they are correct");
+            std::process::exit(1);
+        }
+    };
 
-    println!("{}", get_short_url(url));
+    let url = match value {
+        Some(url) => { url }
+        None => {
+            println!("An error occured while processing the parameters given. Please make sure, they are correct");
+            std::process::exit(1);
+        }
+    };
 
-    Ok(())
+    println!("Shortening your URL \"{}\"...", url);
+
+    let short_url = match get_short_url(url) {
+        Some(url) => { url }
+        None => {
+            println!("An error occured while processing your request. Please make sure, the url is valid");
+            std::process::exit(1);
+        }
+    };
+
+    println!("Your shortened URL is: {}", short_url);
 }
 
-fn get_short_url(long_url: &str) -> String {
+fn get_short_url(long_url: &str) -> Option<String> {
 
     let base_api_url = "http://tinyurl.com/create.php";
 
     let mut body = HashMap::new(); 
     body.insert("url", long_url);
 
-    let client = reqwest::blocking::Client::new();
-    let res = client.post(base_api_url)
+    let client = Client::new();
+    let result = client.post(base_api_url)
         .json(&body)
-        .send()
-        .unwrap();
-    
-    res.text().unwrap()
+        .send();
+
+
+    let response = match result {
+        Ok(response) => { response }
+        Err(_) => {
+            println!("An error occured while processing your request. Please make sure, the url is valid");
+            std::process::exit(1);
+        }
+    };
+
+    if response.status().is_success() {
+        
+        match response.text() {
+            Ok(val) => Some(val),
+            Err(_) => None
+        }
+
+    } else {
+        None
+    }
 }
